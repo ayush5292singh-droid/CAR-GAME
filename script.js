0eed46f9fe670ffe58fc1eef447a7bda8bd61350
Ayush Singh <ayush5292singh@gmail.com>
2026-07-24 14:32:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,153 @@ ground.rotation.x = -Math.PI/2;
 
 
 scene.add(ground);
+// ROAD
 
+let roadGeometry = new THREE.PlaneGeometry(
+8,
+200
+);
+
+
+let roadMaterial = new THREE.MeshPhongMaterial({
+color:0x333333
+});
+
+
+let road = new THREE.Mesh(
+roadGeometry,
+roadMaterial
+);
+
+
+road.rotation.x = -Math.PI/2;
+
+road.position.y = 0.02;
+
+scene.add(road);
+
+
+
+// ROAD LINES
+
+for(let i=0;i<20;i++){
+
+let lineGeometry =
+new THREE.BoxGeometry(
+0.2,
+0.02,
+5
+);
+
+
+let lineMaterial =
+new THREE.MeshBasicMaterial({
+color:0xffffff
+});
+
+
+let line =
+new THREE.Mesh(
+lineGeometry,
+lineMaterial
+);
+
+
+line.position.set(
+0,
+0.04,
+-i*10
+);
+
+
+scene.add(line);
+
+}
+
+
+
+// TREES
+
+function createTree(x,z){
+
+let trunkGeometry =
+new THREE.CylinderGeometry(
+0.2,
+0.2,
+2
+);
+
+
+let trunkMaterial =
+new THREE.MeshPhongMaterial({
+color:0x8B4513
+});
+
+
+let trunk =
+new THREE.Mesh(
+trunkGeometry,
+trunkMaterial
+);
+
+
+trunk.position.set(
+x,
+1,
+z
+);
+
+
+scene.add(trunk);
+
+
+
+let leavesGeometry =
+new THREE.SphereGeometry(
+1
+);
+
+
+let leavesMaterial =
+new THREE.MeshPhongMaterial({
+color:0x00aa00
+});
+
+
+let leaves =
+new THREE.Mesh(
+leavesGeometry,
+leavesMaterial
+);
+
+
+leaves.position.set(
+x,
+2.3,
+z
+);
+
+
+scene.add(leaves);
+
+}
+
+
+// Add trees
+
+for(let i=0;i<20;i++){
+
+createTree(
+-6,
+-i*10
+);
+
+createTree(
+6,
+-i*10
+);
+
+}
 
 // Camera position
 
